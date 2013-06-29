a1a7fb77c3477307da9c0aff8805eac42a7ef71e
Florian Holzapfel <florian.holzapfel@glueckkanja.com>
2013-06-29 20:20:27 +0200

    renamed example folder to examples moved todos example to examples/todos added invoicing example from readme.md

---
+++ examples/invoicing.js
@@ -0,0 +1,54 @@
+/**
+ * invoicing.js
+ *
+ * Copyright (C) 2013 by Florian Holzapfel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+**/
+var http = require('http');
+var express = require('express');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var restify = require('..');
+
+mongoose.connect('mongodb://localhost/database');
+
+var Customer = new Schema({
+	name: { type: String, required: true },
+	comment: { type: String }
+});
+var CustomerModel = mongoose.model('Customer', Customer);
+
+var Invoice = new Schema({
+    customer: { type: Schema.Types.ObjectId, ref: 'Customer' },
+	amount: { type: Number, required: true }
+});
+var InvoiceModel = mongoose.model('Invoice', Invoice);
+
+var app = express();
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	restify.serve(app, CustomerModel);
+	restify.serve(app, InvoiceModel);
+});
+
+http.createServer(app).listen(3000, function() {
+	console.log("Express server listening on port 3000");
+});
