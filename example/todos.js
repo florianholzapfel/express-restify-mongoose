03167547f17be51b7bce939401fc9de3cd6c1c88
Jonathan Greenemeier <Jon@6edesign.net>
2013-07-04 20:38:16 -0600

    Added support for querying count (ie. api/v1/todos/count/?done=true #=> {count: integer}).

--- example/todos.js
+++ example/todos.js
@@ -37,7 +37,7 @@ var ToDoModel = mongoose.model('ToDo', ToDoSchema);
 
 var app = express();
 app.configure(function(){
-	app.set('port', process.env.PORT || 3000);
+	app.set('port', process.env.PORT || 3001);
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.errorHandler());
