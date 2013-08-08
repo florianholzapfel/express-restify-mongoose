13c13bd5816669bbfeca65eb02909bd813bfcaaa
Florian Holzapfel <florian.holzapfel@glueckkanja.com>
2013-08-08 21:56:05 +0200

    better test organization

--- test/test.js
+++ test/test.js
@@ -2,220 +2,213 @@ var erm = require('../lib/express-restify-mongoose');
 
 var setup = require('./setup');
 
-var assert   = require('assertmessage'),
-    express  = require('express'),
+var assert = require('assertmessage'),
+    express = require('express'),
     mongoose = require('mongoose'),
-    restify  = require('restify'),
-    request  = require('request'),
-    Schema   = mongoose.Schema;
+    restify = require('restify'),
+    request = require('request'),
+    Schema = mongoose.Schema;
 
 var util = require('util');
 
 require('sugar');
 
 var testPort = 30023,
-    testUrl  = 'http://localhost:' + testPort;
+    testUrl = 'http://localhost:' + testPort;
 
-var expressApp = express();
-expressApp.use(express.bodyParser());
-expressApp.use(express.methodOverride());
-expressApp.describe = 'Express';
+function Express() {
+    var app = express();
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    return app;
+}
+function Restify() {
+    var app = restify.createServer();
+    app.use(restify.bodyParser());
+    return app;
+}
 
-var restifyApp = restify.createServer();
-restifyApp.use(restify.bodyParser());
-restifyApp.describe = 'Restify';
+[Express, Restify].each(function (createFn) {
+    describe(createFn.name, function () {
+        describe('General', function () {
+            var app = createFn();
+            var savedCustomer, server;
+            setup();
 
-[expressApp, restifyApp].each(function (app) {
-    describe(app.describe, function () {
-
-        var savedCustomer, server;
-        setup();
-
-        before(function (done) {
-            erm.serve(app, setup.customerModel);
-            server = app.listen(testPort, done);
-        });
-
-        after(function (done) {
-            mongoose.connection.close(done);
-        });
-        after(function (done) {
-            if (app.close) {
-                return app.close(done);
-            }
-            server.close(done);
-        });
-
-        it('200 get Customers', function (done) {
-            request.get({
-                url: util.format('%s/api/v1/Customers', testUrl)
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-                done();
+            before(function (done) {
+                erm.serve(app, setup.customerModel);
+                server = app.listen(testPort, done);
             });
-        });
 
-        it('200 get Customers/count', function (done) {
-            request.get({
-                url: util.format('%s/api/v1/Customers/count', testUrl),
-                json: true
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-                assert.equal(body.count, 0, 'Wrong count');
-                done();
+            after(function (done) {
+                mongoose.connection.close(done);
             });
-        });
-
-        it('200 post Customers', function (done) {
-            request.post({
-                url: util.format('%s/api/v1/Customers', testUrl),
-                json: {
-                    name: 'Test',
-                    comment: 'Comment'
+            after(function (done) {
+                if (app.close) {
+                    return app.close(done);
                 }
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-                assert.equal(body.name, 'Test');
-                assert.equal(body.comment, 'Comment');
-                savedCustomer = body;
-                done();
+                server.close(done);
             });
-        });
 
-        it('200 GET Customers?name=Test', function (done) {
-            request.get({
-                url: util.format('%s/api/v1/Customers?name=Test', testUrl),
-                json: true
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-                assert.equal(body.length, 1,
-					'Wrong count of customers returned');
-                assert.deepEqual(savedCustomer, body[0]);
-                done();
+            it('200 get Customers', function (done) {
+                request.get({
+                    url: util.format('%s/api/v1/Customers', testUrl)
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    done();
+                });
             });
-        });
 
-        it('200 GET Customers/:id', function (done) {
-            request.get({
-                url: util.format('%s/api/v1/Customers/%s', testUrl,
+            it('200 get Customers/count', function (done) {
+                request.get({
+                    url: util.format('%s/api/v1/Customers/count', testUrl),
+                    json: true
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    assert.equal(body.count, 0, 'Wrong count');
+                    done();
+                });
+            });
+
+            it('200 post Customers', function (done) {
+                request.post({
+                    url: util.format('%s/api/v1/Customers', testUrl),
+                    json: {
+                        name: 'Test',
+                        comment: 'Comment'
+                    }
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    assert.equal(body.name, 'Test');
+                    assert.equal(body.comment, 'Comment');
+                    savedCustomer = body;
+                    done();
+                });
+            });
+
+            it('200 GET Customers?name=Test', function (done) {
+                request.get({
+                    url: util.format('%s/api/v1/Customers?name=Test', testUrl),
+                    json: true
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    assert.equal(body.length, 1,
+                        'Wrong count of customers returned');
+                    assert.deepEqual(savedCustomer, body[0]);
+                    done();
+                });
+            });
+
+            it('200 GET Customers/:id', function (done) {
+                request.get({
+                    url: util.format('%s/api/v1/Customers/%s', testUrl,
                     savedCustomer._id),
-                json: true
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-                assert.deepEqual(savedCustomer, body);
-                done();
+                    json: true
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    assert.deepEqual(savedCustomer, body);
+                    done();
+                });
+            });
+
+            it('200 PUT Customers/:id', function (done) {
+                savedCustomer.name = 'Test 2';
+                request.put({
+                    url: util.format('%s/api/v1/Customers/%s', testUrl,
+                    savedCustomer._id),
+                    json: savedCustomer
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    assert.deepEqual(savedCustomer, body);
+                    done();
+                });
+            });
+
+            it('200 DEL Customers/:id', function (done) {
+                request.del({
+                    url: util.format('%s/api/v1/Customers/%s', testUrl,
+                    savedCustomer._id),
+                    json: true
+                }, function (err, res) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    done();
+                });
+            });
+
+            it('404s on deleted Customers/:id', function (done) {
+                request.get({
+                    url: util.format('%s/api/v1/Customers/%s', testUrl,
+                    savedCustomer._id),
+                    json: true
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 404, 'Wrong status code');
+                    done();
+                });
             });
         });
 
-        it('200 PUT Customers/:id', function (done) {
-            savedCustomer.name = 'Test 2';
-            request.put({
-                url: util.format('%s/api/v1/Customers/%s', testUrl,
-                    savedCustomer._id),
-                json: savedCustomer
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-                assert.deepEqual(savedCustomer, body);
-                done();
-            });
-        });
+        describe('Excluded comment field', function () {
+            var app = createFn();
+            var savedCustomer, server;
+            setup();
 
-        it('200 DEL Customers/:id', function (done) {
-            request.del({
-                url: util.format('%s/api/v1/Customers/%s', testUrl,
-                    savedCustomer._id),
-                json: true
-            }, function (err, res) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-                done();
-            });
-        });
+            before(function (done) {
+                erm.serve(app, setup.customerModel, {
+                    exclude: 'comment'
+                });
+                server = app.listen(testPort, function () {
+                    request.post({
+                        url: util.format('%s/api/v1/Customers', testUrl),
+                        json: {
+                            name: 'Test',
+                            comment: 'Comment'
+                        }
+                    }, function (err, res, body) {
+                        savedCustomer = body;
 
-        it('404s on deleted Customers/:id', function (done) {
-            request.get({
-                url: util.format('%s/api/v1/Customers/%s', testUrl,
+                        done();
+                    });
+                });
+            });
+
+            after(function (done) {
+                mongoose.connection.close(done);
+            });
+            after(function (done) {
+                if (app.close) {
+                    return app.close(done);
+                }
+                server.close(done);
+            });
+
+            it('200 get Customers', function (done) {
+                request.get({
+                    url: util.format('%s/api/v1/Customers', testUrl),
+                    json: true
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    assert.ok(body[0], 'no items found');
+                    assert.ok(body[0].comment === undefined,
+                        'comment is not undefined');
+
+                    done();
+                });
+            });
+
+            it('200 GET Customers/:id', function (done) {
+                request.get({
+                    url: util.format('%s/api/v1/Customers/%s', testUrl,
                     savedCustomer._id),
-                json: true
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 404, 'Wrong status code');
-                done();
+                    json: true
+                }, function (err, res, body) {
+                    assert.equal(res.statusCode, 200, 'Wrong status code');
+                    assert.ok(body, 'no item found');
+                    assert.deepEqual(savedCustomer, body);
+                    assert.ok(body.comment === undefined,
+                        'comment is not undefined');
+                    done();
+                });
             });
         });
     });
 });
-
-var expressApp = express();
-expressApp.use(express.bodyParser());
-expressApp.use(express.methodOverride());
-expressApp.describe = 'Express';
-
-var restifyApp = restify.createServer();
-restifyApp.use(restify.bodyParser());
-restifyApp.describe = 'Restify';
-
-[expressApp, restifyApp].each(function (app) {
-    describe(app.describe + ' with excluded comment field', function () {
-
-        var savedCustomer, server;
-        setup();
-
-        before(function (done) {
-            erm.serve(app, setup.customerModel, {
-				exclude: 'comment'
-			});
-            server = app.listen(testPort, function () {
-				request.post({
-					url: util.format('%s/api/v1/Customers', testUrl),
-					json: {
-						name: 'Test',
-						comment: 'Comment'
-					}
-				}, function (err, res, body) {
-					savedCustomer = body;
-
-					done();
-				});
-			});
-        });
-
-        after(function (done) {
-            mongoose.connection.close(done);
-        });
-        after(function (done) {
-            if (app.close) {
-                return app.close(done);
-            }
-            server.close(done);
-        });
-
-        it('200 get Customers', function (done) {
-            request.get({
-                url: util.format('%s/api/v1/Customers', testUrl),
-                json: true
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-				assert.ok(body[0], 'no items found');
-				assert.ok(body[0].comment === undefined,
-					'comment is not undefined');
-
-                done();
-            });
-        });
-
-        it('200 GET Customers/:id', function (done) {
-            request.get({
-                url: util.format('%s/api/v1/Customers/%s', testUrl,
-                    savedCustomer._id),
-                json: true
-            }, function (err, res, body) {
-                assert.equal(res.statusCode, 200, 'Wrong status code');
-				assert.ok(body, 'no item found');
-                assert.deepEqual(savedCustomer, body);
-				assert.ok(body.comment === undefined,
-					'comment is not undefined');
-                done();
-            });
-        });
-    });
-});
-
