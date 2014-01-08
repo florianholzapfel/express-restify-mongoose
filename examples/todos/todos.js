0efbddaab079c6377ff8317b71c24922d578aaa0
Florian Holzapfel <fholzapfel@gmx.de>
2014-01-08 19:42:53 +0100

    remove some jshint errors

--- examples/todos/todos.js
+++ examples/todos/todos.js
@@ -36,22 +36,22 @@ var ToDoSchema = new mongoose.Schema({
 var ToDoModel = mongoose.model('ToDo', ToDoSchema);
 
 var app = express();
-app.configure(function(){
+app.configure(function () {
 	app.set('port', process.env.PORT || 3000);
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.errorHandler());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	restify.serve(app, ToDoModel, { 
+	restify.serve(app, ToDoModel, {
 		//exclude: 'text,done'
 	});
 	app.use(express.static(path.join(__dirname, 'public')));
-	app.use(function(req, res) {
+	app.use(function (req, res) {
 		res.sendfile(path.join(__dirname, 'public/index.html'));
 	});
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-	console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
 });
