c594b8c03fd02825cd04656d6aaf4aa8740c325c
Pier-Luc Gendreau <pier_taz@yahoo.com>
2015-09-27 17:27:37 -0400

    chore(tests): remove case for removed feature

--- test/integration/test.js
+++ test/integration/test.js
@@ -461,18 +461,6 @@ module.exports = function (createFn) {
         })
       })
 
-      it('400 GET Products?projection=badJSON', function (done) {
-        var query = 'badJSON'
-        request.get({
-          url: util.format('%s/api/v1/Products?query=%s', testUrl, query),
-          json: true
-        }, function (err, res, body) {
-          assert.ok(!err)
-          assert.equal(res.statusCode, 400, 'Wrong status code')
-          done()
-        })
-      })
-
       it('200 GET Invoices?populate=customer should populate customer', function (done) {
         request.get({
           url: util.format('%s/api/v1/Invoices', testUrl),
