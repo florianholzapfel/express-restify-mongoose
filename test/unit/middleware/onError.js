2becf257789e30060afe59e34f29089a8e1fa6f5
Pier-Luc Gendreau <pier_taz@yahoo.com>
2015-10-07 15:54:06 -0400

    Fix unit tests

--- test/unit/middleware/onError.js
+++ test/unit/middleware/onError.js
@@ -4,51 +4,43 @@ describe('onError', function () {
   var onError = require('../../../lib/middleware/onError')
 
   var res = {
-    setHeader: function () {},
-    status: function () {
+    sendStatus: function () {
       return this
     },
-    json: function () {},
     send: function () {}
   }
 
-  var setHeader = sinon.spy(res, 'setHeader')
-  var status = sinon.spy(res, 'status')
+  var sendStatus = sinon.spy(res, 'sendStatus')
   var send = sinon.spy(res, 'send')
-  var json = sinon.spy(res, 'json')
   var next = sinon.spy()
 
-  var err = new Error('An error occurred')
-  err.statusCode = 400
-
   afterEach(function () {
-    setHeader.reset()
-    status.reset()
+    sendStatus.reset()
     send.reset()
-    json.reset()
     next.reset()
   })
 
   it('with express', function () {
+    var err = new Error('An error occurred')
+    err.statusCode = 400
+
     onError(true)(err, {}, res, next)
 
-    sinon.assert.calledOnce(setHeader)
-    sinon.assert.calledWithExactly(setHeader, 'Content-Type', 'application/json')
-    sinon.assert.calledOnce(status)
-    sinon.assert.calledWithExactly(status, err.statusCode)
-    sinon.assert.calledOnce(json)
-    sinon.assert.calledWithExactly(json, err)
+    sinon.assert.calledOnce(sendStatus)
+    sinon.assert.calledWithExactly(sendStatus, 400)
+    sinon.assert.notCalled(send)
     sinon.assert.notCalled(next)
   })
 
   it('with restify', function () {
+    var err = new Error('An error occurred')
+    err.statusCode = 400
+
     onError(false)(err, {}, res, next)
 
-    sinon.assert.calledOnce(setHeader)
-    sinon.assert.calledWithExactly(setHeader, 'Content-Type', 'application/json')
-    sinon.assert.notCalled(status)
     sinon.assert.calledOnce(send)
-    sinon.assert.calledWithExactly(send, err.statusCode, JSON.parse(JSON.stringify(err)))
+    sinon.assert.calledWithExactly(send, 400, 'Bad Request')
+    sinon.assert.notCalled(sendStatus)
     sinon.assert.notCalled(next)
   })
 })
