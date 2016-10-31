a806e3c23f85a8ce7d269c003b9f5d337ac7053c
b-gran <bill@billgranfield.com>
2016-10-31 16:37:34 -0400

    Add docs to buildQuery

--- src/buildQuery.js
+++ src/buildQuery.js
@@ -1,50 +1,62 @@
 const Promise = require('bluebird')
 
-module.exports = function (options) {
-  return function (query, queryOptions) {
+/**
+ * Given global (ERM-instance-wide) query-building options, returns a function that extends a
+ * partial query with local (request-specific) query options.
+ *
+ * @param {Object} globalOptions - ERM-instance query options
+ * @param {Number} globalOptions.limit -
+ * @param {String} globalOptions.readPreference -
+ * @param {Boolean} globalOptions.lean -
+ *
+ * @return {function(ModelQuery, Object): Promise}
+ */
+module.exports = function (globalOptions) {
+  // Note: requestSpecificQueryOptions should be the result of a prepareQuery() call
+  return function (query, requestSpecificQueryOptions) {
     const promise = new Promise((resolve, reject) => {
-      if (!queryOptions) {
+      if (!requestSpecificQueryOptions) {
         return resolve(query)
       }
 
-      if (queryOptions.query) {
-        query.where(queryOptions.query)
+      if (requestSpecificQueryOptions.query) {
+        query.where(requestSpecificQueryOptions.query)
       }
 
-      if (queryOptions.skip) {
-        query.skip(queryOptions.skip)
+      if (requestSpecificQueryOptions.skip) {
+        query.skip(requestSpecificQueryOptions.skip)
       }
 
-      if (options.limit && (!queryOptions.limit || queryOptions.limit === '0' || queryOptions.limit > options.limit)) {
-        queryOptions.limit = options.limit
+      if (globalOptions.limit && (!requestSpecificQueryOptions.limit || requestSpecificQueryOptions.limit === '0' || requestSpecificQueryOptions.limit > globalOptions.limit)) {
+        requestSpecificQueryOptions.limit = globalOptions.limit
       }
 
-      if (queryOptions.limit && query.op !== 'count' && !queryOptions.distinct) {
-        query.limit(queryOptions.limit)
+      if (requestSpecificQueryOptions.limit && query.op !== 'count' && !requestSpecificQueryOptions.distinct) {
+        query.limit(requestSpecificQueryOptions.limit)
       }
 
-      if (queryOptions.sort) {
-        query.sort(queryOptions.sort)
+      if (requestSpecificQueryOptions.sort) {
+        query.sort(requestSpecificQueryOptions.sort)
       }
 
-      if (queryOptions.populate) {
-        query.populate(queryOptions.populate)
+      if (requestSpecificQueryOptions.populate) {
+        query.populate(requestSpecificQueryOptions.populate)
       }
 
-      if (queryOptions.select) {
-        query.select(queryOptions.select)
+      if (requestSpecificQueryOptions.select) {
+        query.select(requestSpecificQueryOptions.select)
       }
 
-      if (queryOptions.distinct) {
-        query.distinct(queryOptions.distinct)
+      if (requestSpecificQueryOptions.distinct) {
+        query.distinct(requestSpecificQueryOptions.distinct)
       }
 
-      if (options.readPreference) {
-        query.read(options.readPreference)
+      if (globalOptions.readPreference) {
+        query.read(globalOptions.readPreference)
       }
 
-      if (options.lean) {
-        query.lean(options.lean)
+      if (globalOptions.lean) {
+        query.lean(globalOptions.lean)
       }
 
       resolve(query)
