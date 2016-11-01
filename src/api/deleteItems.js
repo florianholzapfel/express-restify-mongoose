c5ef5f1de87b518acc5cb89e07612911724bcf1f
b-gran <bill@billgranfield.com>
2016-11-01 12:55:06 -0400

    Promisify deleteItems

--- src/api/deleteItems.js
+++ src/api/deleteItems.js
@@ -1,24 +1,52 @@
+const getQueryBuilder = require('../buildQuery')
+const APIMethod = require('../APIMethod')
+
 /**
- * Given a model and erm options, returns Express middleware that builds a mongoose query to
- * delete items from the database and executes it.
+ * Given global query options, a Mongoose context (just a ModelQuery), and a Mongo
+ * query, removes the documents in the context that match the Mongo query.
  *
- * @param {Object} model
- * @param {Object} options
- * @return {function(*=, *=, *=)}
+ * @param {Object} queryOptions - Global options to apply to all queries
+ * @param queryOptions.lean
+ * @param queryOptions.readPreference
+ * @param queryOptions.limit
+ *
+ * @param {ModelQuery} mongooseContext - The documents to query
+ * @param {Object} query - MongoDB query object to apply to the context
+ *
+ * @return {Promise}
  */
-function deleteItems (model, options) {
-  const buildQuery = require('../buildQuery')(options)
-  const errorHandler = require('../errorHandler')(options)
-
-  return (req, res, next) => {
-    options.contextFilter(model, req, (filteredContext) => {
-      buildQuery(filteredContext.remove(), req._ermQueryOptions).then(() => {
-        req.erm.statusCode = 204
-
-        next()
-      }, errorHandler(req, res, next))
-    })
-  }
+function deleteItems (queryOptions, mongooseContext, query) {
+  const buildQuery = getQueryBuilder(queryOptions)
+  return buildQuery(mongooseContext.remove(), query)
 }
 
-module.exports = deleteItems
+/**
+ * Delete all of the items specified by a query in an Express request.
+ *
+ * @param {ERMInstance} ermInstance
+ * @param {Object} req
+ * @return {Promise}
+ */
+function deleteItemsWithRequest (ermInstance, req) {
+  // Explicit construction because contextFilter() takes a callback
+  return new Promise((resolve, reject) => {
+    ermInstance.options.contextFilter(
+      ermInstance.model,
+      req,
+      filteredContext => {
+        deleteItems(ermInstance.options, filteredContext, req._ermQueryOptions)
+          .then(resolve({
+            erm: {
+              statusCode: 204
+            }
+          }))
+          .catch(err => reject(err))
+      }
+    )
+  })
+}
+
+module.exports = new APIMethod(
+  deleteItems,
+  deleteItemsWithRequest
+)
