9dd88579830e12c4e788eb29382e782d653e9a98
b-gran <bill@billgranfield.com>
2016-11-11 17:07:53 -0500

    Implements getShallow() as APIMethod

--- src/api/database-operations/getShallow.js
+++ src/api/database-operations/getShallow.js
@@ -1,40 +1,80 @@
 const http = require('http')
 const findById = require('./../shared').findById
+const getQueryBuilder = require('../../buildQuery')
+
+const APIMethod = require('../../APIMethod')
 
 /**
- * Given a model, erm options, and a list of excluded keys for model descendants, returns
- * Express middleware that builds a mongoose query to retrieve a _single_ item (specified by
- * its id), executes the query, and stores the result (a single document) in the request.
+ * Given global query options, a Mongoose context (just a ModelQuery), and a Mongo
+ * query, shallowly retrieve a single document with the correct id (and id property) that is also in
+ * the supplied context and query.
  *
  * This operation is similar to getItem(), except it flattens nested object properties of the
  * document into a single truthy value
  *
- * @param {Object} model
- * @param {Object} options
- * @return {function(*=, *=, *=)}
+ * @param {Object} queryOptions - Global options to apply to all queries
+ * @param queryOptions.lean
+ * @param queryOptions.readPreference
+ * @param queryOptions.limit
+ *
+ * @param {ModelQuery} mongooseContext - The documents to query
+ * @param {Object} query - MongoDB query object to apply to the context
+ *
+ * @param {String} documentId - the document's id
+ * @param {String} idProperty - the model's id property
+ *
+ * @return {Promise<Object>}
  */
-function getShallow (model, options) {
-  const buildQuery = require('../../buildQuery')(options)
-  const errorHandler = require('../../errorHandler')(options)
+function getShallow (queryOptions, mongooseContext, query, documentId, idProperty) {
+  const buildQuery = getQueryBuilder(queryOptions)
 
-  return (req, res, next) => {
-    options.contextFilter(model, req, (filteredContext) => {
-      buildQuery(findById(filteredContext, req.params.id, options.idProperty), req._ermQueryOptions).then((item) => {
-        if (!item) {
-          return errorHandler(req, res, next)(new Error(http.STATUS_CODES[404]))
-        }
+  return buildQuery(
+    findById(mongooseContext, documentId, idProperty),
+    query
+  ).then(item => {
+    // Strip object properties
+    for (let prop in item) {
+      item[prop] = typeof item[prop] === 'object' && prop !== '_id' ? true : item[prop]
+    }
 
-        for (let prop in item) {
-          item[prop] = typeof item[prop] === 'object' && prop !== '_id' ? true : item[prop]
-        }
-
-        req.erm.result = item
-        req.erm.statusCode = 200
-
-        next()
-      }, errorHandler(req, res, next))
-    })
-  }
+    return item
+  })
 }
 
-module.exports = getShallow
+/**
+ * Shallowly retrieve a single item matching a query inside some consumer-provided context.
+ *
+ * Rejects with a 404 error if a document with that id isn't found in the context.
+ *
+ * @param {ERMOperation} state
+ * @param {Object} req
+ * @return {Promise<ERMOperation>}
+ */
+function getShallowWithRequest (state, req) {
+  // Explicit construction because contextFilter() takes a callback
+  return new Promise((resolve, reject) => {
+    state.options.contextFilter(state.model, req,
+      filteredContext => {
+        getShallow(state.options, filteredContext, state.query, req.params.id, state.options.idProperty)
+          .then(shallowItem => {
+            // If the query succeeds but no document is found, return 404
+            if (!shallowItem) {
+              return Promise.reject(new Error(http.STATUS_CODES[404]))
+            }
+
+            return resolve(
+              state
+                .setResult(shallowItem)
+                .setStatusCode(200)
+            )
+          })
+          .catch(err => reject(err))
+      }
+    )
+  })
+}
+
+module.exports = new APIMethod(
+  getShallow,
+  getShallowWithRequest
+)
--- src/operations.js
+++ src/operations.js
@@ -1,7 +1,7 @@
 module.exports = function (ermInstance) {
   const getItems = require('./api/database-operations/getItems').getMiddleware(ermInstance)
   const getCount = require('./api/database-operations/getCount').getMiddleware(ermInstance)
-  const getShallow = require('./api/database-operations/getShallow')(ermInstance.model, ermInstance.options)
+  const getShallow = require('./api/database-operations/getShallow').getMiddleware(ermInstance)
   const deleteItems = require('./api/database-operations/deleteItems').getMiddleware(ermInstance)
   const getItem = require('./api/database-operations/getItem').getMiddleware(ermInstance)
   const deleteItem = require('./api/database-operations/deleteItem').getMiddleware(ermInstance)
