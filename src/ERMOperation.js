cb0819b03f8fef73d217e5d1dcab7dc3f7a2fbe5
b-gran <bill@billgranfield.com>
2017-01-25 23:49:36 -0500

    Adds correct JSDoc type annotations

--- src/ERMOperation.js
+++ src/ERMOperation.js
@@ -1,76 +1,123 @@
-const _ = require('lodash')
+/**
+ * @module './ERMOperation'
+ */
+
 const ImmutableRecord = require('immutable-record')
+const _ = require('lodash')
 const Model = require('mongoose').Model
 
-// Underlying record for the Operation class
-const OperationRecord = ImmutableRecord({
-  context: {
-    type: ctx => _.isFunction(ctx) || _.isObject(ctx)
+/**
+ * Underlying record for the Operation class
+ */
+const OperationRecord = ImmutableRecord(
+  /** @lends ERMOperation */
+  {
+    /**
+     * Query representing the document(s) we want to operate on
+     * @alias module:ERMOperation#context
+     * @type {function|Object}
+     */
+    context: {
+      type: ctx => _.isFunction(ctx) || _.isObject(ctx)
+    },
+
+    /**
+     * The permissions granted to whoever is doing the operation
+     * @alias module:ERMOperation#accessLevel
+     * @type {String}
+     */
+    accessLevel: {
+      type: 'string'
+    },
+
+    /**
+     * The HTTP status code of the operation, if finished
+     * @alias module:ERMOperation#statusCode
+     * @type {Number}
+     */
+    statusCode: {
+      type: 'number'
+    },
+
+    /**
+     * Total count of documents in the operation, if finished
+     * @alias module:ERMOperation#totalCount
+     * @type {Number}
+     */
+    totalCount: {
+      type: 'number'
+    },
+
+    /**
+     * The result of the operation, if applicable
+     * @alias module:ERMOperation#result
+     * @type {Array|Object}
+     */
+    result: {},
+
+    /**
+     * If the operation operates on a single document, this gets set
+     * @alias module:ERMOperation#document
+     * @type {Object}
+     */
+    document: {
+      type: 'object'
+    },
+
+    /**
+     * Object that represents the MongoDB query for the operation
+     * @alias module:ERMOperation#query
+     * @type {Object}
+     */
+    query: {
+      type: 'object'
+    },
+
+    /**
+     * Top-level ERM options for the operation.
+     * @alias module:ERMOperation#options
+     * @type {Object}
+     */
+    options: {
+      type: 'object',
+      required: true
+    },
+
+    /**
+     * The mongoose Model we're operating on
+     * @alias module:ERMOperation#model
+     * @type {mongoose.Model}
+     */
+    model: {
+      type: isModel,
+      required: true
+    },
+
+    /**
+     * Descendant keys to filter out
+     * @alias module:ERMOperation#excludedMap
+     * @type {Object}
+     */
+    excludedMap: {
+      type: 'object',
+      required: true
+    }
   },
-
-  accessLevel: {
-    type: 'string'
-  },
-
-  statusCode: {
-    type: 'number'
-  },
-
-  totalCount: {
-    type: 'number'
-  },
-
-  result: {},
-
-  document: {
-    type: 'object'
-  },
-
-  query: {
-    type: 'object'
-  },
-
-  options: {
-    type: 'object',
-    required: true
-  },
-
-  model: {
-    type: isModel,
-    required: true
-  },
-
-  excludedMap: {
-    type: 'object',
-    required: true
-  }
-}, 'ERMOperation')
+  'ERMOperation'
+)
 
 /**
  * An immutable data structure that represents an in-progress ERM operation.
- *
- * @property {Object} model - The mongoose Model we're operating on
- * @property {Object} options - Consumer-specified options
- * @property {Object} excludedMap - Descendant keys to filter out
- * @property {ModelQuery} context - Mongoose query to search
- * @property {String} accessLevel - The permissions granted to whoever is doing the operation
- *
- * @property {Number} statusCode - The HTTP status code of the operation, if finished
- * @property {Number} totalCount - Total count of documents in the operation, if finished
- * @property {Array|Object} result - The result of the operation, if applicable
- * @property {Object} document - If the operation operates on a single document, this gets set
- *
- * @property {Object} query - Object that represents the MongoDB query for the operation
- *
- * @property serializeToRequest
- * @property set
- * @property remove
+ * @alias module:ERMOperation
+ * @class
  */
 class ERMOperation extends OperationRecord {
+
   /**
    * Return an object that can be stored on an Express request object to persist ERMOperation
    * state in between middleware.
    *
+   * @alias module:ERMOperation#serializeToRequest
    * @return {Object}
    */
   serializeToRequest () {
@@ -95,6 +142,8 @@ class ERMOperation extends OperationRecord {
 
 /**
  * Given an Express request, deserializes an ERMOperation from it.
+ *
+ * @alias module:ERMOperation.deserializeRequest
  *
  * @param {Object} req - the Express request
  * @return {ERMOperation}
@@ -124,9 +173,11 @@ ERMOperation.deserializeRequest = function (req) {
  * Initialize a new ERMOperation with a model, options, and an excludedMap.
  * All parameters are required.
  *
- * @property {Object} model - The mongoose Model we're operating on
- * @property {Object} options - Consumer-specified options
- * @property {Object} excludedMap - Descendant keys to filter out
+ * @alias module:ERMOperation.initialize
+ *
+ * @param {Object} model - The mongoose Model we're operating on
+ * @param {Object} options - Consumer-specified options
+ * @param {Object} excludedMap - Descendant keys to filter out
  *
  * @return {ERMOperation}
  */
