3d9e9a890f65e8862b8c9d4021e76ce3d748f3c1
Pier-Luc Gendreau <pierluc@outlook.com>
2022-10-23 14:09:20 -0400

    Update invoicing example

--- examples/invoicing.js
+++ examples/invoicing.js
@@ -1,34 +1,37 @@
-var http = require('http')
-var express = require('express')
-var bodyParser = require('body-parser')
-var methodOverride = require('method-override')
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
-var restify = require('..')
+import bodyParser from "body-parser";
+import express from "express";
+import http from "http";
+import methodOverride from "method-override";
+import mongoose from "mongoose";
+import { serve } from "../src/express-restify-mongoose";
 
-mongoose.connect('mongodb://localhost/database', {
+mongoose.connect("mongodb://localhost/database", {
   useMongoClient: true,
-})
+});
 
-var Customer = new Schema({
+const Customer = new mongoose.Schema({
   name: { type: String, required: true },
   comment: { type: String },
-})
-var CustomerModel = mongoose.model('Customer', Customer)
+});
 
-var Invoice = new Schema({
-  customer: { type: Schema.Types.ObjectId, ref: 'Customer' },
+const CustomerModel = mongoose.model("Customer", Customer);
+
+const Invoice = new mongoose.Schema({
+  customer: { type: Schema.Types.ObjectId, ref: "Customer" },
   amount: { type: Number, required: true },
-})
-var InvoiceModel = mongoose.model('Invoice', Invoice)
+});
 
-var app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(methodOverride('X-HTTP-Method-Override'))
-restify.serve(app, CustomerModel)
-restify.serve(app, InvoiceModel)
+const InvoiceModel = mongoose.model("Invoice", Invoice);
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride("X-HTTP-Method-Override"));
+
+serve(app, CustomerModel);
+serve(app, InvoiceModel);
 
 http.createServer(app).listen(3000, function () {
-  console.log('Express server listening on port 3000')
-})
+  console.log("Express server listening on port 3000");
+});
